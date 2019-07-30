4c5ef57fe29db3317cfd5119def4f0a8ef85fcd8
hwj <feiyu_u@163.com>
2019-07-30 17:06:11 +0800

    first  commit

---
+++ app/src/main/java/com/example/feiyu/gifview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.feiyu.gifview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GifView gif_view = findViewById(R.id.gif_view);
+
+        gif_view.setGifResource(R.mipmap.demo1);
+        gif_view.play();
+    }
+}
